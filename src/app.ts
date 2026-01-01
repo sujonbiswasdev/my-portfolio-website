e10bb1616062a42ec7b05cad391d5a73c5e0b940
Sujon Biswas <sujonbiswasdev@gmail.com>
2026-01-01 05:45:42 +0000

    done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import { postRouter } from './modules/post/post.route'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 const app=express()
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 // post route
 app.use("/posts",postRouter.router)
 app.get('/', (req, res) => {
