f49db2bb43e8d063644768c559d445b80fc0296e
Sujon Biswas <sujonbiswasdev@gmail.com>
2026-01-01 06:07:35 +0000

    done

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import { postRouter } from './modules/post/post.route'
 import { toNodeHandler } from "better-auth/node";
 import { auth } from './lib/auth';
+import cors from 'cors'
 const app=express()
-
+// middleware
+app.use(cors({origin:process.env.APP_URL}))
 app.all("/api/auth/*splat", toNodeHandler(auth));
 // post route
 app.use("/posts",postRouter.router)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,34 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    trustedOrigins:[process.env.APP_URL!],
+    user:{
+        additionalFields:{
+            role:{
+                type:"string",
+                defaultValue:"USER",
+                required:false
+            },
+            phone:{
+                type:"string",
+                required:false
+            },
+            status:{
+                type:"string",
+                defaultValue:"ACTIVE",
+                required:false
+            }
+        }
+    },
     emailAndPassword: { 
     enabled: true, 
+    autoSignIn:false,
+    requireEmailVerification:true
   }, 
+
+  emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+     console.log("********verification email send")
+    },
+  },
 });
